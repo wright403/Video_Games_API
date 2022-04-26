b1b519a8d4b5258cc2887573cce4ee82fd83870c
fred <fredwright95@yahoo.com>
2022-04-26 12:45:50 -0400

    created get all and get by id endpoints

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,39 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    //api/Games
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetPublishers()
+        {
+            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Publisher).Distinct();
+
+            return Ok(videoGamePublishers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesByPublisher(int id)
+        {
+            
+                var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+                return Ok(videoGames);
+            
+        }
+        
+     }
+}
